9f070e2ea520eda8f0f46839e682ed01ce4d5750
woopen <nepow@foxmail.com>
2020-12-17 17:55:26 +0800

    feat: Add api extractor

--- packages/grafeo/src/index.ts
+++ packages/grafeo/src/index.ts
@@ -1,1 +1,17 @@
-export default 'asd';
+export default class {
+  private a: number;
+  private b: string;
+
+  constructor(a: number, b: string) {
+    this.a = a;
+    this.b = b;
+  }
+
+  getA(): number {
+    return this.a;
+  }
+
+  getB(): string {
+    return this.b;
+  }
+}
