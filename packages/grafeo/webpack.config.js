e831bcf54b48511bcb661063657cc4edbb0cec7a
woopen <nepow@foxmail.com>
2020-12-17 16:22:52 +0800

    feat: Add website & setup grafeo

---
+++ packages/grafeo/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+/**@type {import('webpack').Configuration} */
+module.exports = {
+  mode: 'production',
+  bail: true,
+  devtool: 'source-map',
+
+  entry: './src/index.ts',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
